19ffe2db52a355eb5878c969e08b427a9cd76a0d
emanucamargo <134521892+emanucamargo@users.noreply.github.com>
2023-08-23 10:40:28 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const disciplina = new Array ('matematica','geografia','portugues','biologia','historia','fisica','quiimica')
+disciplina.splice (7,2)
+disciplina.splice (7,5)
+
+const colegio = new Array ('eliasAbrahao','bandeirantes','arlinda','andreAndreata','campoSales')
+colegio.splice (5,3)
+
+const time = new Array ('flamengo','corinthians','vasco','fluminense','santos','coritiba','gremio','botafogo'.'atlheticoPr','saoPaulo')
+time.splice (10,8)
