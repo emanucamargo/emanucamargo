b62c3b3ba756e60e9974e598349b2ccd36c7aa05
emanucamargo <134521892+emanucamargo@users.noreply.github.com>
2024-10-22 08:17:50 -0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-const disciplina = new Array ('matematica','geografia','portugues','biologia','historia','fisica','quiimica')
-disciplina.splice (7,2)
-disciplina.splice (7,5)
-
-const colegio = new Array ('eliasAbrahao','bandeirantes','arlinda','andreAndreata','campoSales')
-colegio.splice (5,3)
-
-const time = new Array ('flamengo','corinthians','vasco','fluminense','santos','coritiba','gremio','botafogo'.'atlheticoPr','saoPaulo')
-time.splice (10,8)
